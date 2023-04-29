33eda0d508aeb11dfcd62c4556ada406e453bd07
morganluke <morganluke@gmail.com>
2023-04-29 15:51:27 -0700

    added app.js, contacts.js

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-const awesomeFunction = (req, res, next) => {
-    res.json('Mike Little');
-};
-
-const returnAnotherPerson = (req, res, next) => {
-    res.json('Super awesome person');
-};
-
-module.exports = {awesomeFunction, returnAnotherPerson};
