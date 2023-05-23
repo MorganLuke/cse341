543afa6f1a108abfc77c830afdfe95af23e8b5ba
morganluke <morganluke@gmail.com>
2023-05-22 23:14:13 -0700

    changed host swagger.js

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Contacts API'
   },
-  host: 'lukecse341.onrender.com',
+  host: 'https://lukecse341.onrender.com/',
   schemes: ['https']
 };
 
@@ -13,9 +13,9 @@ const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
 // generate swagger.json
-// swaggerAutogen(outputFile, endpointsFiles, doc);
+swaggerAutogen(outputFile, endpointsFiles, doc);
 
 // Run server after it gets generated
-swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
-  await import('./index.js');
-});
+// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+//   await import('./index.js');
+// });
